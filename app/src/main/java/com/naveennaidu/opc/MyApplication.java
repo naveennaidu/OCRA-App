eddd79fa77b98104ca3e5cf32b957f2f68e8d1b8
naveennaidu <mnaveennaidu9927@gmail.com>
2019-02-13 19:24:22 +0530

    Added Cloud Database and Cloud Storage

---
+++ app/src/main/java/com/naveennaidu/opc/MyApplication.java
@@ -0,0 +1,10 @@
+package com.naveennaidu.opc;
+
+import android.app.Application;
+
+
+public class MyApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+    }
+}
